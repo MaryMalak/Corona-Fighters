8f5d3966c6df03ae8c1c017bd40c4bf5164efb49
MariamGhaly1 <41520000+MariamGhaly1@users.noreply.github.com>
2021-06-16 01:06:39 +0200

    main

---
+++ main.py
@@ -0,0 +1,107 @@
+import cv2
+import imutils
+from centroidtracker import Mass_Centre
+from itertools import combinations
+import math
+import numpy as np
+import datetime
+import Module
+
+protopath = "MobileNetSSD_deploy.prototxt"
+modelpath = "MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
+
+
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+tracker = Mass_Centre(max_frames=10, limited_length=50)
+def main(path):
+    cap = cv2.VideoCapture(path)
+
+    fps_start_time = datetime.datetime.now()
+    fps = 0
+    total_frames = 0
+
+    while True:
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=800)
+        total_frames = total_frames + 1
+
+        (H, W) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+
+        detector.setInput(blob)
+        person_detections = detector.forward()
+        drawn_rectangles = []
+        for i in np.arange(0, person_detections.shape[2]):
+            confidence = person_detections[0, 0, i, 2]
+            if confidence > 0.5:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] != "person":
+                    continue
+
+                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (startX, startY, endX, endY) = person_box.astype("int")
+                drawn_rectangles.append(person_box)
+
+        boundingboxes = np.array(drawn_rectangles)
+        boundingboxes = boundingboxes.astype(int)
+        drawn_rectangles = Module.non_max_suppression_fast(boundingboxes, 0.3)
+        dict_centre = dict()
+        objs = tracker.modify(drawn_rectangles)
+        for (obj_identifier, bbox) in objs.items():
+            x1, y1, x2, y2 = bbox
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            cX = int((x1 + x2) / 2.0)
+            cY = int((y1 + y2) / 2.0)
+
+
+            dict_centre[obj_identifier] = (cX, cY, x1, y1, x2, y2)
+
+            # text = "ID: {}".format(obj_identifier)
+            # cv2.putText(frame, text, (x1, y1-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+        red_zone_list = []
+        for (id1, p1), (id2, p2) in combinations(dict_centre.items(), 2):
+            dx, dy = p1[0] - p2[0], p1[1] - p2[1]
+            distance = math.sqrt(dx * dx + dy * dy)
+            if distance < 75.0:
+                if id1 not in red_zone_list:
+                    red_zone_list.append(id1)
+                if id2 not in red_zone_list:
+                    red_zone_list.append(id2)
+
+        for id, box in dict_centre.items():
+            if id in red_zone_list:
+                cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 0, 255), 2)
+            else:
+                cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2)
+
+
+        fps_end_time = datetime.datetime.now()
+        time_diff = fps_end_time - fps_start_time
+        if time_diff.seconds == 0:
+            fps = 0.0
+        else:
+            fps = (total_frames / time_diff.seconds)
+
+        fps_text = "FPS: {:.2f}".format(fps)
+
+        cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+        cv2.imshow("Application", frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
