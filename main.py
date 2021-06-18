980ff2815a426645023b0fe44c395867caafd6b8
tinafadi99 <62008369+tinafadi99@users.noreply.github.com>
2021-06-18 12:51:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ modelpath = "MobileNetSSD_deploy.caffemodel"
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
 
 
-
+# To specify objs to be detected 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
@@ -29,7 +29,7 @@ def main(path):
 
     while True:
         ret, frame = cap.read()
-        # put old data ina array with new size
+        # Put old frame data in array with new size
         frame = imutils.resize(frame, width=800)
         total_frames = total_frames + 1
 
@@ -41,6 +41,7 @@ def main(path):
         person_detections = detector.forward()
         drawn_rectangles = []
            
+        # To ensure person detection using classes array
         for i in np.arange(0, person_detections.shape[2]):
             confidence = person_detections[0, 0, i, 2]
             if confidence > 0.5:
@@ -48,11 +49,12 @@ def main(path):
 
                 if CLASSES[idx] != "person":
                     continue
-
+           
                 person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                 (startX, startY, endX, endY) = person_box.astype("int")
                 drawn_rectangles.append(person_box)
-        # Recall non max suppression function for isolating stronger edges and thinning
+                      
+        # Recall non max suppression function for stronger edges isolation and thinning
         boundingboxes = np.array(drawn_rectangles)
         boundingboxes = boundingboxes.astype(int)
         drawn_rectangles = Module.non_max_suppression_fast(boundingboxes, 0.3)
@@ -70,20 +72,24 @@ def main(path):
 
             dict_centre[obj_identifier] = (cX, cY, x1, y1, x2, y2)
 
-
+        # Create list to accumulate red rectangles ids 
         red_zone_list = []
         for (id1, p1), (id2, p2) in combinations(dict_centre.items(), 2):
             dx, dy = p1[0] - p2[0], p1[1] - p2[1]
             distance = math.sqrt(dx * dx + dy * dy)
+           # If there's unsafe distance, create red rectangle
             if distance < 75.0:
+                # Add the red rectangle if it's not already existed                   
                 if id1 not in red_zone_list:
                     red_zone_list.append(id1)
                 if id2 not in red_zone_list:
                     red_zone_list.append(id2)
 
         for id, box in dict_centre.items():
+           # To specify red color of rectangle
             if id in red_zone_list:
                 cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 0, 255), 2)
+           # To specify green color of rectangle
             else:
                 cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2)
 
