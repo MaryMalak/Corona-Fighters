9a264b5a5e3c13514e40e53d56c77f278cf7bc59
tinafadi99 <62008369+tinafadi99@users.noreply.github.com>
2021-06-19 10:45:56 +0200

    Update _add_obj.py

--- _add_obj.py
+++ _add_obj.py
@@ -1,8 +1,8 @@
 # obj registration
 def add_obj(self, centroid, inputRect):
     # Put the center
+    # Using the identifier of following accessible obj to register
     self.objs[self.following_obj_ID] = centroid
     self.bbox[self.following_obj_ID] = inputRect  
     self.gone[self.following_obj_ID] = 0
-    # Using the identifier of following accessible obj to register
     self.following_obj_ID += 1
